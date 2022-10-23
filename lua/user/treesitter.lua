b60db6abe45ed8ec0fcf832b34d8c4d82cf6dc81
jeremy.ling <jeremy.ling@cmexpro.com>
2022-10-23 14:23:27 +0000

    Add clangd config and treesitter highlight

--- lua/user/treesitter.lua
+++ lua/user/treesitter.lua
@@ -31,7 +31,7 @@ config.setup {
     disable = { "javascript" },
     -- Or use a function for more flexibility, e.g. to disable slow treesitter highlight for large files
     disable = function(lang, buf)
-        local max_filesize = 100 * 1024 -- 100 KB
+        local max_filesize = 10 * 1024 * 1024 -- 10 MB
         local ok, stats = pcall(vim.loop.fs_stat, vim.api.nvim_buf_get_name(buf))
         if ok and stats and stats.size > max_filesize then
             return true
